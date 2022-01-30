55e75adc1e208667f6c1e66d4ede4c4e15c28f23
Johannes <betz90@posteo.de>
2022-01-30 22:09:34 +0100

    add lesson 1 code, opencv not yet working

---
+++ src/TrafficObject.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <chrono>
+#include "TrafficObject.h"
+
+// Task L1.1 : In the base class TrafficObject, 
+// set up a thread barrier in its destructor that ensures that all the thread objects in the member vector _threads are joined
+
+// init static variable
+int TrafficObject::_idCnt = 0;
+
+void TrafficObject::setPosition(double x, double y)
+{
+    _posX = x;
+    _posY = y;
+}
+
+void TrafficObject::getPosition(double &x, double &y)
+{
+    x = _posX;
+    y = _posY;
+}
+
+TrafficObject::TrafficObject()
+{
+    _type = ObjectType::noObject;
+    _id = _idCnt++;
+}
+
+TrafficObject::~TrafficObject()
+{
+    // Task L1.1 : Set up a thread barrier that ensures that all the thread objects in the member vector _threads are joined.
+    for (auto &t : _threads){
+        t.join();
+    }
+}
